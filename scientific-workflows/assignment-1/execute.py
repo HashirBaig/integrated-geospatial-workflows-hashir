4842a030726b03b106abdd57433f58d349657cfe
HashirBaig <hashirbaig456@gmail.com>
2025-05-08 01:34:41 +0200

    added geojson for assignment 3

--- scientific-workflows/assignment-1/execute.py
+++ scientific-workflows/assignment-1/execute.py
@@ -4,7 +4,7 @@ import requests, os
 
 payload = open(os.path.dirname(os.path.abspath(__file__)) +"\\xml\coords_extraction.xml").read()
  
-BASES_SERVER_URL = "https://gisedu.itc.utwente.nl/student/s3381730/gpw/assignment-1/api" or "https://mara.rangelands.itc.utwente.nl/geoserver/ows?"
+BASES_SERVER_URL = "https://gisedu.itc.utwente.nl/student/s3381730/gpw/assignment-1/api"
 API_URL = (BASES_SERVER_URL+"/wps.py?")
 
 
